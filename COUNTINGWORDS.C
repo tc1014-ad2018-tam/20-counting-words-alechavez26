9fad77966c9d7beb966c5557bec48530cda8171e
Alejandra Chávez Cruz <42187730+alechavez26@users.noreply.github.com>
2018-10-18 23:25:58 -0500

    Add files via upload

---
+++ COUNTINGWORDS.C
@@ -0,0 +1,35 @@
+﻿/*
+ * This program counts the number of words that are in a string.
+ *
+ * Alejandra Chávez Cruz
+ * A01411970@itesm.mx
+ * 18/oct/18
+ */
+#include <stdio.h>
+
+
+
+    int main() {
+        //Variables
+        char string[100];
+        int cont= 1;
+        int words=0;
+
+
+            // the program ask to the user for a string
+            printf(" write a string: ");
+            fgets(string, sizeof(string), stdin);
+
+
+            while (string[cont] != '\0') {
+                cont++;
+                if (string[cont] == ' '){
+                    words++;
+                }
+            }
+
+            //the result
+            printf("Your string has %i words.", words);
+
+            return 0;
+        }
